7b9d422ad353784c23738e551aa8777c6e2ec671
palomatejedor <166176848+palomatejedor@users.noreply.github.com>
2025-12-01 21:38:01 -0300

    Delete script.js directory

--- script.js/script.js
+++
@@ -1,8 +0,0 @@
-// Efecto de agrandar imagen al hacer clic
-const imagenesZoom = document.querySelectorAll(".zoom");
-
-imagenesZoom.forEach((img) => {
-  img.addEventListener("click", () => {
-    img.classList.toggle("activa");
-  });
-});
